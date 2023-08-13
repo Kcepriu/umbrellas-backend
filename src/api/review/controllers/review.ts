751a5e7c785d1c1d7ad3c5da71af16662e8895c5
Kcepriu <kcepriu@gmail.com>
2023-08-13 21:16:46 +0300

    add review

--- src/api/review/controllers/review.ts
+++ src/api/review/controllers/review.ts
@@ -2,6 +2,26 @@
  * review controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::review.review');
+export default factories.createCoreController(
+  "api::review.review",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const query = ctx.request.query;
+      const data = ctx.request.body;
+      const files = ctx.request.files;
+
+      data.data.user = ctx.state.user.id;
+      if (!data.data.date) data.data.date = Date.now();
+
+      const results = await strapi
+        .service("api::review.review")
+        .create({ ...query, ...data, files });
+
+      const sanitizedResults = await this.sanitizeOutput(results, ctx);
+
+      return this.transformResponse(sanitizedResults, {});
+    },
+  })
+);
--- src/api/review/routes/review.ts
+++ src/api/review/routes/review.ts
@@ -2,6 +2,28 @@
  * review router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::review.review');
+export default factories.createCoreRouter("api::review.review", {
+  config: {
+    find: {},
+    findOne: {},
+    create: {},
+    update: {
+      middlewares: [
+        {
+          name: "global::is-owner ",
+          config: { uid: "api::review.review" },
+        },
+      ],
+    },
+    delete: {
+      middlewares: [
+        {
+          name: "global::is-owner ",
+          config: { uid: "api::review.review" },
+        },
+      ],
+    },
+  },
+});
